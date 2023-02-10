6f0ec803e87cead6b3fa405042c1dd4601b2a2dc
Christian Dionisio <christiandionisio9432@gmail.com>
2023-02-10 16:52:59 -0500

    cat column section

--- src/components/catpage/CatPageInfoSection.jsx
+++ src/components/catpage/CatPageInfoSection.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export const CatPageInfoSection = () => {
   return (
     <div className="catpage-info-section">
-      <div className="catpage-info-column">
+      <div className="catpage-info-column1">
         <img src="https://cdn2.thecatapi.com/images/4-5SzDNIL.jpg" alt="cat_image" />
       </div>
-      <div className="catpage-info-column">
+      <div className="catpage-info-column2">
         <h2>Bengal</h2>
         <p>
           Bengals are a lot of fun to live with, but the're defintely not the
@@ -22,7 +22,7 @@ export const CatPageInfoSection = () => {
         <h6><strong>Life Span: </strong> 12 - 15 years</h6>
         <div className="catpage-list-features">
           <div className="catpage-cat-feature">
-            <strong>Adaptability:</strong> 
+            <strong>Adaptability:</strong>
             <div className="catpage-level-rate">
               <div className="catpage-level"></div>
               <div className="catpage-level"></div>
@@ -33,7 +33,7 @@ export const CatPageInfoSection = () => {
           </div>
 
           <div className="catpage-cat-feature">
-            <strong>Affection level:</strong> 
+            <strong>Affection level:</strong>
             <div className="catpage-level-rate">
               <div className="catpage-level"></div>
               <div className="catpage-level"></div>
@@ -55,7 +55,7 @@ export const CatPageInfoSection = () => {
           </div>
 
           <div className="catpage-cat-feature">
-            <strong>Grooming:</strong> 
+            <strong>Grooming:</strong>   
             <div className="catpage-level-rate">
               <div className="catpage-level"></div>
               <div className="catpage-level"></div>
@@ -66,7 +66,7 @@ export const CatPageInfoSection = () => {
           </div>
 
           <div className="catpage-cat-feature">
-            <strong>Intelligence: </strong> 
+            <strong>Intelligence:</strong>
             <div className="catpage-level-rate">
               <div className="catpage-level"></div>
               <div className="catpage-level"></div>
@@ -77,7 +77,7 @@ export const CatPageInfoSection = () => {
           </div>
 
           <div className="catpage-cat-feature">
-            <strong>Health issues</strong> 
+            <strong>Health issues</strong>
             <div className="catpage-level-rate">
               <div className="catpage-level"></div>
               <div className="catpage-level"></div>
