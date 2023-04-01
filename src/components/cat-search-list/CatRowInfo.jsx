c95a07a7985b62cc99015aa3c4b7448b9f3556b0
Christian Dionisio <christiandionisio9432@gmail.com>
2023-04-01 18:53:56 -0500

    CatRowInfo.css added

--- src/components/cat-search-list/CatRowInfo.jsx
+++ src/components/cat-search-list/CatRowInfo.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import  '../../styles/components/cat-search-list/CatRowInfo.css'
 
 export const CatRowInfo = () => {
   return (
-    <div className='cat-search-row-list'>
-        <div className="cat-search-row-list column-1">
+    <div className='cat-search-row'>
+        <div className="col-1">
             <img src="https://cdn2.thecatapi.com/images/4-5SzDNIL.jpg" alt="cat-image" />
         </div>
 
-        <div className="cat-search-row-list column-2">
+        <div className="col-2">
             <h2>1. Bengal</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus similique sequi voluptatem a mollitia quasi. Aliquid consequuntur sequi accusamus, veritatis at corrupti, commodi nihil quis, odit nemo debitis voluptatum libero.</p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Temporibus similique sequi voluptatem a mollitia quasi. 
+              Aliquid consequuntur sequi accusamus, veritatis at corrupti, 
+              commodi nihil quis, odit nemo debitis voluptatum libero.
+            </p>
         </div>
 
     </div>
