8a3717035ad87ae0edd7ea5c47d834c8baf1d420
Christian Dionisio <christiandionisio9432@gmail.com>
2023-04-03 20:30:53 -0500

    add navigate

--- src/components/cat-search-list/CatRowInfo.jsx
+++ src/components/cat-search-list/CatRowInfo.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import  '../../styles/components/cat-search-list/CatRowInfo.css'
+import { useNavigate } from 'react-router-dom';
 
 export const CatRowInfo = ({catInfo, index}) => {
 
   const [catInfoBreed, setCatInfoBreed] = useState(catInfo.breeds[0]);
+  const navigate = useNavigate();
+
+  const goToCatPage = (id) => {
+    navigate(`/cat/${id}`);
+}
   
   return (
     <div className='cat-search-row animate__animated animate__fadeIn animate__delay-1s'>
-        <div className="col-1">
+        <div className="col-1" onClick={() => goToCatPage(catInfoBreed.id)}>
             <img src={catInfo.url} alt="cat-image" />
         </div>
 
